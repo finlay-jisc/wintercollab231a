943a9884446bd32090741f72663dd3c9ba8edeb9
Finlay Jisc <finlay.birnie@jisc.ac.uk>
2023-12-06 10:56:38 +0000

    add pin icon and remove unused component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbTack } from '@fortawesome/free-solid-svg-icons'
 // eslint-disable-next-line react/prop-types
 const Product = ({ name, icon: Icon, href, lpad, setPinned, colour }) => {
     const [isPinned, setIsPinned] = useState(false);
@@ -6,6 +8,7 @@ const Product = ({ name, icon: Icon, href, lpad, setPinned, colour }) => {
     return (
         <>
             <a href={href} style={{color: colour || "inherit"}} className="font-semibold text-gray-500">
+                <FontAwesomeIcon className="pr-4" icon={faThumbTack} />
                 {name}
             </a>
     </>
